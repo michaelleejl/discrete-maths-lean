d0e81731a3e0c7e5787c03a596133f037c696e54
michaelleejl <michaelleejl@gmail.com>
2026-02-28 13:55:58 +0800

    add up to division theorem

---
+++ lectures/Numbers1.lean
@@ -0,0 +1,167 @@
+import Mathlib.Data.Nat.Basic
+import Mathlib.Logic.ExistsUnique
+
+open Nat
+
+----------------------------- Lecture 05 -----------------------------
+
+-- Definition 43
+structure Monoid (X : Type) where
+  e : X
+  op : X → X → X
+  l_neu : ∀ x : X, op e x = x
+  r_neu : ∀ x : X, op x e = x
+  assoc : ∀ x y z : X, op (op x y) z = op x (op y z)
+
+def ListMonoid (a : Type) : Monoid (List a) where
+  e := []
+  op := List.append
+  l_neu := List.nil_append
+  r_neu := List.append_nil
+  assoc := List.append_assoc
+
+def NatAddMonoid : Monoid Nat where
+  e := 0
+  op := Nat.add
+  l_neu := Nat.zero_add
+  r_neu := Nat.add_zero
+  assoc := Nat.add_assoc
+
+def NatMulMonoid : Monoid Nat where
+  e := 1
+  op := Nat.mul
+  l_neu := Nat.one_mul
+  r_neu := Nat.mul_one
+  assoc := Nat.mul_assoc
+
+-- Definition 43b
+structure CommutativeMonoid (X : Type) extends Monoid X where
+ comms : ∀ x y : X, op x y = op y x
+
+def NatAddCommMonoid : CommutativeMonoid Nat :=
+  { NatAddMonoid with
+    comms := Nat.add_comm }
+
+
+def NatMulCommMonoid : CommutativeMonoid Nat :=
+  { NatMulMonoid with
+    comms := Nat.mul_comm }
+
+----------------------------- Lecture 06 -----------------------------
+
+-- Definition 44
+structure Semiring (X : Type) where
+  plus : CommutativeMonoid X
+  mul    : Monoid X
+  dest_l : ∀ x : X, mul.op plus.e x = plus.e
+  dest_r : ∀ x : X, mul.op x plus.e = plus.e
+  dist_l : ∀ x y z : X,
+    mul.op x (plus.op y z) = plus.op (mul.op x y) (mul.op x z)
+  dist_r : ∀ y z x : X,
+    mul.op (plus.op y z) x = plus.op (mul.op y x) (mul.op z x)
+
+structure CommutativeSemiring (X : Type) where
+  plus : CommutativeMonoid X
+  mul    : CommutativeMonoid X
+  dest_l : ∀ x : X, mul.op plus.e x = plus.e
+  dest_r : ∀ x : X, mul.op x plus.e = plus.e
+  dist_l : ∀ x y z : X,
+    mul.op x (plus.op y z) = plus.op (mul.op x y) (mul.op x z)
+  dist_r : ∀ y z x : X,
+    mul.op (plus.op y z) x = plus.op (mul.op y x) (mul.op z x)
+
+def NatAddMulCommutativeSemiring : CommutativeSemiring Nat where
+  plus := NatAddCommMonoid
+  mul  := NatMulCommMonoid
+  dest_l := Nat.zero_mul
+  dest_r := Nat.mul_zero
+  dist_l := Nat.mul_add
+  dist_r := Nat.add_mul
+
+-- Definition 45
+def cancellable_left {X : Type} (op : X → X → X) (c : X) :=
+  ∀ x y : X, op c x = op c y → x = y
+
+def cancellable_right {X : Type} (op : X → X → X) (c : X) :=
+  ∀ x y : X, op x c = op y c → x = y
+
+def cancellable {X : Type} (op : X → X → X) (c : X) :=
+  cancellable_left op c ∧ cancellable_right op c
+
+example {X : Type} : ∀ (xs : List X), cancellable (List.append) xs
+  := by intro xs
+        dsimp [cancellable, cancellable_left, cancellable_right]
+        constructor
+        · intros x y
+          exact List.append_cancel_left
+        · intros x y
+          exact List.append_cancel_right
+
+def monoid_left_inverse {X : Type} (M : Monoid X) (l x : X)
+  := M.op l x = M.e
+
+def monoid_right_inverse {X : Type} (M : Monoid X) (x r : X)
+  := M.op x r = M.e
+
+def monoid_inverse {X : Type} (M : Monoid X) (x y : X)
+  := monoid_left_inverse M y x ∧ monoid_right_inverse M x y
+
+-- Proposition 47
+theorem uniqueness_of_monoid_inverse {X : Type} :
+    ∀ (M : Monoid X) (x l r : X),
+    monoid_left_inverse M l x ∧ monoid_right_inverse M x r →
+    l = r
+  := by intros M x l r
+        dsimp [monoid_left_inverse, monoid_right_inverse]
+        intros h
+        match h with
+          | ⟨ hl, hr ⟩ => calc
+                          l = M.op l M.e        := Eq.symm (M.r_neu l)
+                          _ = M.op l (M.op x r) := by rw [hr]
+                          _ = M.op (M.op l x) r := Eq.symm (M.assoc l x r)
+                          _ = M.op M.e r        := by rw [hl]
+                          _ = r                 := M.l_neu r
+
+
+structure Group (X : Type) extends Monoid X where
+  invers : ∀ x : X, ∃ y : X, monoid_inverse (toMonoid) x y
+
+structure AbelianGroup (X : Type) extends CommutativeMonoid X where
+  invers : ∀ x : X, ∃ y : X, monoid_inverse (toMonoid) x y
+
+-- Definition 50
+def admits_additive_inverse (n : ℕ) : Prop := ∃ m : ℕ, n + m = 0
+
+example : admits_additive_inverse 0
+  := by exists 0
+
+def admits_multiplicative_inverse (n : ℕ) : Prop := ∃ m : ℕ, n * m = 1
+
+example : admits_multiplicative_inverse 1
+  := by exists 1
+
+-- Definition 51
+structure Ring (X : Type) where
+  plus : AbelianGroup X
+  mul    : Monoid X
+  dest_l : ∀ x : X, mul.op plus.e x = plus.e
+  dest_r : ∀ x : X, mul.op x plus.e = plus.e
+  dist_l : ∀ x y z : X,
+    mul.op x (plus.op y z) = plus.op (mul.op x y) (mul.op x z)
+  dist_r : ∀ y z x : X,
+    mul.op (plus.op y z) x = plus.op (mul.op y x) (mul.op z x)
+
+structure CommutativeRing (X : Type) where
+  plus : AbelianGroup X
+  mul    : CommutativeMonoid X
+  dest_l : ∀ x : X, mul.op plus.e x = plus.e
+  dest_r : ∀ x : X, mul.op x plus.e = plus.e
+  dist_l : ∀ x y z : X,
+    mul.op x (plus.op y z) = plus.op (mul.op x y) (mul.op x z)
+  dist_r : ∀ y z x : X,
+    mul.op (plus.op y z) x = plus.op (mul.op y x) (mul.op z x)
+
+structure Field (X : Type) extends CommutativeRing X where
+  CR : CommutativeRing X
+  recipro : ∀ x : X, x ≠ CR.plus.e
+    → ∃ y: X, monoid_inverse CR.mul.toMonoid x y
---
+++ lectures/Numbers2.lean
@@ -0,0 +1,80 @@
+import Mathlib.Data.Nat.Basic
+import Mathlib.Logic.ExistsUnique
+import Mathlib.Data.Nat.ModEq
+
+import Mathlib.Tactic
+
+open Nat
+
+----------------------------- Lecture 05 -----------------------------
+
+-- Theorem 53, Definition 54, Theorem 56
+structure QuoRem (m n : ℕ) where
+  q : ℕ
+  r : ℕ
+  eq : m = q * n + r
+  lt : r < n
+
+
+def division_algorithm : (m : ℕ) → (n: ℕ) → (n > 0) → QuoRem m n :=
+  fun m => fun n => fun p =>
+    if hle : m ≤ n then
+      if hlt : m < n
+        then
+          have eq : m = 0 * n + m := by simp
+          have lt : m < n := hlt
+          ⟨ 0, m, eq, lt⟩
+        else
+          have heq : m = n := Nat.le_antisymm hle (Nat.not_lt.mp hlt)
+          have eq : m = 1 * n + 0 := by simp [heq]
+          have lt : 0 < n := p
+          ⟨ 1 , 0, eq, lt ⟩
+    else
+      have h : n ≤ m := Nat.le_of_lt (Nat.gt_of_not_le hle)
+      let ⟨ q, r, heq, hlt ⟩ := division_algorithm (m - n) n p;
+      have eq : m = (q + 1) * n + r
+        := calc
+            m = (m - n) + n := by rw [Nat.sub_add_cancel h]
+            _ = q * n + r + n := by rw [heq]
+            _ = q * n + (r + n) := by rw [Nat.add_assoc]
+            _ = q * n + (n + r) := by rw [Nat.add_comm r n]
+            _ = (q * n + n) + r  := by rw [Nat.add_assoc]
+            _ = (q + 1) * n + r := by rw [← Nat.succ_mul]
+      ⟨q + 1, r, eq, hlt⟩
+
+theorem division_theorem {m n : ℕ} :
+    n > 0 → ∃!qr : ℕ × ℕ, qr.2 < n ∧ m = qr.1 * n + qr.2
+  := by intro hpos
+        let ⟨ q, r, eq, lt ⟩ := division_algorithm m n hpos
+        refine ⟨(q, r), ?existence, ?uniqueness⟩
+        · exact ⟨ lt, eq ⟩
+        · intro ⟨ q', r' ⟩
+          dsimp
+          intro h
+          match h with
+          | ⟨ hr, hm ⟩ =>
+                have h1 : m - r  = n * q
+                  := by rw [eq, Nat.add_sub_cancel, Nat.mul_comm]
+                have h2 : m - r' = n * q'
+                  := by rw [hm, Nat.add_sub_cancel, Nat.mul_comm]
+                have h3 : r ≡ m [MOD n]
+                  := by rw [Nat.modEq_iff_dvd]
+                        exists q
+                        linarith [h1]
+                have h4 : r' ≡ m [MOD n]
+                  := by rw [Nat.modEq_iff_dvd]
+                        exists q'
+                        linarith [h2]
+                have h5 : r' ≡ r [MOD n]
+                  := Nat.ModEq.trans h4 (Nat.ModEq.symm h3)
+                have h6 : r' = r
+                  := Nat.ModEq.eq_of_lt_of_lt h5 hr lt
+                have h7 : q' = q
+                  := by rw [h6] at hm
+                        rw [hm] at eq
+                        rw [Nat.add_left_inj] at eq
+                        rw [Nat.mul_left_inj (Nat.ne_of_gt hpos)] at eq
+                        exact eq
+                apply Prod.ext
+                · exact h7
+                · exact h6
